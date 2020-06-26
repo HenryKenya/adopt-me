ae5838bcf7369e7df9a96565a99f546820992727
Dru <henry.onyango@outlook.com>
2020-06-27 00:11:18 +0300

    create SearchParam component, add useState hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => (
   <div>
     <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Cobra" animal="Dog" breed="German Shephard" />
+    <SearchParams />
   </div>
 );
 
